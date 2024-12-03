57b9c2976a68ebfe974b98a41f396bb4769aa964
Michail <62212858+Michail19@users.noreply.github.com>
2024-12-03 12:15:03 +0300

    Initial commit

---
+++ src/main/java/com/me/App.java
@@ -0,0 +1,16 @@
+package com.me;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    // Метод для тестирования
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
